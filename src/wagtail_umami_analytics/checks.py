b5e5b088f3cc66b69749477afba72063d48cd7dd
lasse-cs <lasse.schmieding@gmail.com>
2026-06-02 23:02:29 +0100

    Fix formatting

--- src/wagtail_umami_analytics/checks.py
+++ src/wagtail_umami_analytics/checks.py
@@ -6,7 +6,7 @@ def check_for_wagtail_settings(app_configs, **kwargs):
     if app_configs is not None:
         if not any(app.label == "wagtail_umami_analytics" for app in app_configs):
             return []
-    
+
     try:
         apps.get_app_config("wagtailsettings")
     except LookupError:
